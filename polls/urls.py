067850fd674dc9b2ec976ccb3e96c455ef5f0e30
GToidZ <39559733+GToidZ@users.noreply.github.com>
2022-09-04 11:41:57 +0700

    Added results and vote views; made votes saved to DB

--- polls/urls.py
+++ polls/urls.py
@@ -6,4 +6,6 @@ app_name = "polls"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:question_id>/", views.details, name="details"),
+    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import Http404
-from .models import Question
+from django.http import Http404, HttpResponseRedirect
+from django.urls import reverse
+from .models import Question, Choice
 
 
 def index(request):
@@ -11,9 +12,41 @@ def index(request):
 
 
 def details(request, question_id):
-    """View for viewing question, choices and results; and for casting votes."""
+    """View for viewing question and its choices."""
     try:
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
     return render(request, "polls/details.html", {"question": question})
+
+
+def vote(request, question_id):
+    """View for casting a vote.
+    What I learned here: view acts like a path on website, you could also
+    use HTTP methods here without showing anything.
+    """
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/details.html",
+            {"question": question, "error_message": "You didn't choose a choice..."},
+        )
+    else:
+        selected_choice.vote_count += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+
+def results(request, question_id):
+    """View for seeing results of a poll."""
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/results.html", {"question": question})
