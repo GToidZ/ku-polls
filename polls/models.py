a0d1774bc471d5ee682f46e3efab02829e6c9e95
GToidZ <39559733+GToidZ@users.noreply.github.com>
2022-09-04 08:57:11 +0700

    Created models for the polls

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=280)
+    publish_date = models.DateTimeField("date published")
+
+
+class Choice(models.Model):
+    # Designed with backtracking relationship
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=80)
+    vote_count = models.IntegerField(default=0)
