cae93927d7e9ecd38980f2937c1e87bea93e8a7c
GToidZ <39559733+GToidZ@users.noreply.github.com>
2022-09-04 09:40:57 +0700

    Created an index view

--- polls/models.py
+++ polls/models.py
@@ -1,9 +1,14 @@
+from datetime import timedelta
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=280)
     publish_date = models.DateTimeField("date published")
+
+    def was_published_recently(self):
+        return self.publish_date >= timezone.now() - timedelta(days=3)
 
 
 class Choice(models.Model):
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Polls app")
+    """View for list of recent questions."""
+    latest_questions = Question.objects.order_by("-publish_date")[:5]
+    ctx = {"latest_questions": latest_questions}
+    return render(request, "polls/index.html", ctx)
