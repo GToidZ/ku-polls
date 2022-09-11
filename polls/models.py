d1cd6b34eaed2d52d0b59ff03b84f042a0e2fe77
GToidZ <39559733+GToidZ@users.noreply.github.com>
2022-09-11 11:45:31 +0700

    Made changes to Question model
    + Added end_date and visibility attributes
    
    + Added can_vote and is_published methods
    
    ! Made migrations to Database

--- polls/models.py
+++ polls/models.py
@@ -8,9 +8,21 @@ class Question(models.Model):
 
     question_text = models.CharField(max_length=280)
     publish_date = models.DateTimeField("date published")
+    end_date = models.DateTimeField("date closed", default=None, null=True)
+    visibilty = models.BooleanField("poll visibility", default=True)
+
+    def is_published(self):
+        if not self.visibilty:
+            return False
+        return timezone.now() >= self.publish_date
 
     def was_published_recently(self):
         return timezone.now() >= self.publish_date >= timezone.now() - timedelta(days=3)
+
+    def can_vote(self):
+        if not end_date:
+            return True
+        return self.end_date >= timezone.now() >= self.publish_date
 
     def __str__(self):
         return f"{self.question_text}"
