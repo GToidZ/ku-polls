3303ff863ee1ed7ae4ba8ef6197a4178c3cf74cc
GToidZ <39559733+GToidZ@users.noreply.github.com>
2022-09-23 17:16:17 +0700

    Modified settings.py to prevent error when there's no env
    + Ignored db.sqlite3
    
    - Removed db.sqlite3 from repo; use setup/fixtures instead

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,11 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=False, cast=bool)
+DEBUG = config("DEBUG", default=True, cast=bool)
 
 ALLOWED_HOSTS = config(
     "ALLOWED_HOSTS",
-    default=["127.0.0.1", "localhost"],
+    default="127.0.0.1,localhost",
     cast=lambda val: [s.strip() for s in val.split(",")],
 )
 
